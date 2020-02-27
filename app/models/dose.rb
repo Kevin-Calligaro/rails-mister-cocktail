9bf0a7b991cea2d2b99a071bb0354ea64de1af35
kevin-calligaro <calligaro.kevin@gmail.com>
2020-02-27 13:09:34 +0100

    associated ok

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true
+  validates :cocktail, presence: true, uniqueness: {scope: :ingredient}
+  validates :ingredient, presence: true, uniqueness: {scope: :cocktail}
+
+end
